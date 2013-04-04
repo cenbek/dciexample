9da4c608f992a4469a138bac123d23f90f88c76a
cenbek <sa.ordekci@gmail.com>
2013-04-04 11:58:54 +0300

    HeadTeacherDefineAcademicYearContextTest file modified.
       - line "class HeadTeacherDefineAcademicYearContextTest < MiniTest::Unit::TestCase"
         replaced with
       + "class HeadTeacherDefineAcademicYearContextTest < MiniTest::Spec"

--- test/contexts/headteacher_define_academicyear_context_test.rb
+++ test/contexts/headteacher_define_academicyear_context_test.rb
@@ -2,7 +2,7 @@ require "minitest_helper"
 require_relative "../../app/models/academicyear"
 require_relative "../../app/models/person"
 require_relative "../../app/contexts/headteacher_define_academicyear_context"
-class HeadTeacherDefineAcademicYearContextTest < MiniTest::Unit::TestCase
+class HeadTeacherDefineAcademicYearContextTest < MiniTest::Spec
   def test_should_add_academic_year
     academicyear=AcademicYear.new 
     academicyear.year="2012-2013"
